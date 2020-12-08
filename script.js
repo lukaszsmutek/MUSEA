df76d6f0ab2ddb961c4de303398a6df289faa6c3
Lukasz Smutek <lukaszsmutek@gmail.com>
2020-12-08 17:54:17 +0100

    Add js function to menu animation

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ inputs.forEach((input) => {
 //hamburger
 
 const hamburger = document.querySelector("#hamburger-menu-container");
-const handleClick=()=>{hamburger.classList.toggle("active");}
+const menu = document.querySelector(".menu-items-container");
+const listItems = document.querySelector(".list-items");
+const listItem = document.querySelectorAll(".list-item");
+
+const menuAnimation=()=>{for(let i=0; i<listItem.length; i++){
+    if(listItem[i].style.transform===(""))
+    { listItem[i].style.transform=("translateY("+[i]*50+"px)");}
+   else{listItem[i].style.transform=("");}
+}}
+
+const handleClick=()=>{hamburger.classList.toggle("active");
+menu.classList.toggle("active");
+menuAnimation();
+}
 
 hamburger.addEventListener('click', handleClick);
+
+//social media scroll
+
