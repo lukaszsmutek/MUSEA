4d57fb811eee70a5de377b08f05ae916e214a5d4
Lukasz Smutek <lukaszsmutek@gmail.com>
2020-12-28 11:22:19 +0100

    Add hero text  scroll-animation

--- script.js
+++ script.js
@@ -56,5 +56,16 @@ const invisible = () => {
     isScroll = setTimeout(visible, 200);
 }
 
-
 window.addEventListener('scroll', invisible, false)
+
+//hero header scroll
+
+const header = document.querySelector(".main__hero-header");
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    header.style.top= ("calc( 50% - " + value*0.5 + "px)");
+    header.style.letterSpacing = ("calc( 0.5em + " + value*0.01 + "em)");
+    //header.style.lineHeight = ("calc( 120% - " + value*0.5 + "%)");
+    header.style.opacity = ("calc( 1 - " + value*0.005 + ")")
+    }
+);
